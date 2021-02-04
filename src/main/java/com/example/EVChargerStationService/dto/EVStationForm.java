e59a42cb19b5a44f42e858ef8c9d1d96bbfdd9af
z159423 <tmdghks992@naver.com>
2021-02-04 17:32:11 +0900

    html 수정 form수정

--- src/main/java/com/example/EVChargerStationService/dto/EVStationForm.java
+++ src/main/java/com/example/EVChargerStationService/dto/EVStationForm.java
@@ -5,5 +5,16 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class EVStationForm {
+
+    private String location;            //설치위치
+    private String roadAddress;         //도로명주소
+    private String latitude;            //위도
+    private String longitude;           //경도
+    private int standardChargerNum;     //급속충전기 대수
+    private int normalChargerNum;       //완속충전기 대수
+    private String supportedVehicles;   //지원차종
 }
--- src/main/java/com/example/EVChargerStationService/dto/RequestEVStationForm.java
+++ src/main/java/com/example/EVChargerStationService/dto/RequestEVStationForm.java
@@ -1,9 +1,23 @@
 package com.example.EVChargerStationService.dto;
 
+import com.example.EVChargerStationService.entity.RequestEVStation;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class RequestEVStationForm {
+
+    private String latitude;            //위도
+    private String longitude;           //경도
+    private String markerComment;             //의견
+    private int recommendationCount;    //추천
+
+    public RequestEVStation toEntity()
+    {
+        return new RequestEVStation(null,latitude,longitude,markerComment,recommendationCount);
+    }
 }
