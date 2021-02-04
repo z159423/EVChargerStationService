6b2dd014799014c25a669d866694af05cdab049d
z159423 <tmdghks992@naver.com>
2021-02-04 21:12:01 +0900

    RequestEVStationMap.html 수정

--- src/main/java/com/example/EVChargerStationService/controller/EVStationController.java
+++ src/main/java/com/example/EVChargerStationService/controller/EVStationController.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +46,30 @@ public class EVStationController {
 
 
         return "/RequestEVStationMap";
+    }
+
+    @GetMapping("/apitest")
+    public String callapihttp() {
+        StringBuffer result = new StringBuffer();
+        try {
+            String urlstr = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?"+
+                    "ServiceKey=MKalVDv7C9R0%2F26oZlr6qIdmvzJToDcLqylzcPVgVnCfW5U%2B9LnvjvI8ojleXwJRnW5g52yxRq9FWLBy3YPRzQ%3D%3D"+
+                    "&pageNo=1"+
+                    "&numOfRows=10";
+            URL url = new URL(urlstr);
+            HttpURLConnection urlconnection = (HttpURLConnection)url.openConnection();
+            urlconnection.setRequestMethod("GET");
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
+            String returnLine;
+            result.append("<xmp>");
+            while((returnLine = br.readLine())!=null) {
+                result.append(returnLine + "\n");
+            }
+            //urlconnection.disconnect();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result+"</xmp>";
+
     }
 }
