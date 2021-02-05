fd3582c07f59e7c86139f1790bfaa2f73e5c7de1
z159423 <tmdghks992@naver.com>
2021-02-05 21:49:35 +0900

    왼쪽 상단 메뉴 이미지 수정 정비소 마커 이미지 변경 EVStation의 칼럼 수정

--- src/main/java/com/example/EVChargerStationService/dto/EVStationForm.java
+++ src/main/java/com/example/EVChargerStationService/dto/EVStationForm.java
@@ -21,4 +21,16 @@ public class EVStationForm {
     private String statUpdDt;            //상태갱신일시   예):20190829121020
     private String busiCall;             //충전기 운영기관 연락처
 
+
+    //기존에 있던 폼
+    /*private String location;            //설치위치
+    private String roadAddress;         //주소
+    private String latitude;            //위도
+    private String longitude;           //경도
+    private String chargerType;         //충전기타입(01:DC차데모,02: AC완속,03: DC차데모+AC3상,04: DC콤보,05: DC차데모+DC콤보, 06: DC차데모+AC3상+DC콤보,07: AC3상)
+
+    private String useTime;              //이용가능시간
+    private int stat;                    //충전기상태(1: 통신이상, 2: 충전대기,3: 충전중, 4: 운영중지,5: 점검중, 9: 상태미확인)
+    private String statUpdDt;            //상태갱신일시   예):20190829121020
+    private String busiCall;             //충전기 운영기관 연락처*/
 }
--- src/main/java/com/example/EVChargerStationService/entity/EVStation.java
+++ src/main/java/com/example/EVChargerStationService/entity/EVStation.java
@@ -46,4 +46,31 @@ public class EVStation {
 
     @Column(name = "BUSICALL")
     private String busiCall;             //충전기 운영기관 연락처
+
+
+    //기존에 있던 entity
+    /*@Id
+    @GeneratedValue
+    Long id;
+
+    @Column(name = "LOCATION")
+    private String location;            //설치장소
+
+    @Column(name = "ROADADDRESS")
+    private String roadAddress;         //도로명주소
+
+    @Column(name = "LATITUDE")
+    private String latitude;            //위도
+
+    @Column(name = "LONGITUDE")
+    private String longitude;           //경도
+
+    @Column(name = "STANDARDCHARGERNUM")
+    private int standardChargerNum;     //급속충전기 대수
+
+    @Column(name = "NORMALCHARGERNUM")
+    private int normalChargerNum;       //완속충전기 대수
+
+    @Column(name = "SUPPORTEDVEHICLES")
+    private String supportedVehicles;   //지원차종*/
 }
